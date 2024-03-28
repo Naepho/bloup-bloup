54b7c775c19c310fa1e818ffa41840167712e027
Naepho <quentin.binstok@gmail.com>
2024-03-28 16:37:46 +0100

    initialized solver and main

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
-print("Hello world!")
+import numpy as np
+import matplotlib.pyplot as plt
+
+import solver
+
+if __name__=="__main__":
+    # Importing files
+    path = "./files"
+    nodes_num = np.loadtxt(path + '/' + "2-num.txt", dtype = int)
+    nodes_dom = np.loadtxt(path + '/' + "2-dom.txt", dtype = int)
+    nodes_cl = np.loadtxt(path + '/' + "1-cl.txt", dtype = float)
+
+    plt.imshow(nodes_dom)
+    plt.show()
